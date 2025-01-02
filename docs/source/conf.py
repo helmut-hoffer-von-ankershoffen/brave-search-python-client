324b4679398e3d821b4258070c9dac0dbe6585dd
Helmut Hoffer von Ankershoffen <helmut@aignostics.com>
2025-01-02 14:32:44 +0100

    Bump version: 0.2.0 → 0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "brave-search-python-client"
 copyright = "2025, Helmut Hoffer von Ankershoffen"
 author = "Helmut Hoffer von Ankershoffen"
-release = "0.2.0"
+release = "0.2.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
