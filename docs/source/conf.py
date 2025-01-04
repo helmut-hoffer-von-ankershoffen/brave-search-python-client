02293f77731bb1ee80622c81d3e38f97f5883fd8
Helmut Hoffer von Ankershoffen <helmut@aignostics.com>
2025-01-04 09:47:12 +0100

    debug: rtd/sphinx/pdf/enums

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,32 @@ import sys
 import os
 import datetime
 
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_toolbox.collapse",  # https://sphinx-toolbox.readthedocs.io/
+    "sphinx_toolbox.sidebar_links",
+    # "sphinx_toolbox.more_autodoc",
+    # "sphinx_toolbox.more_autosummary",
+    "sphinx_toolbox.github",
+    "sphinx_toolbox.source",
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+    "enum_tools.autoenum",  # https://github.com/domdfcoding/enum_tools/tree/master
+    "sphinx.ext.napoleon",  # https://sphinxcontrib-napoleon.readthedocs.io/en/latest/
+    "sphinx-pydantic",
+    "sphinxcontrib.autodoc_pydantic",  # https://autodoc-pydantic.readthedocs.io/en/stable/users/examples.html
+    "sphinx.ext.coverage",
+    "sphinx_copybutton",
+    "sphinx.ext.extlinks",  # https://www.sphinx-doc.org/en/master/usage/extensions/extlinks.html
+    "sphinx.ext.imgconverter",
+    "sphinx_inline_tabs",
+    "sphinx_mdinclude",
+    "sphinxemoji.sphinxemoji",  # https://sphinxemojicodes.readthedocs.io/en/stable/
+    "sphinxext.opengraph",
+]
+
 project = "brave-search-python-client"
 author = "Helmut Hoffer von Ankershoffen"
 copyright = " (c) 2025-{}, {}".format(datetime.datetime.now().year, author)
@@ -30,40 +56,31 @@ ogp_enable_meta_description = True
 ogp_description_length = 300
 
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+autodoc_pydantic_settings_show_json = False
 
-extensions = [
-    "sphinx_toolbox.collapse",      # https://sphinx-toolbox.readthedocs.io/
-    "sphinx_toolbox.sidebar_links",
-    # "sphinx_toolbox.more_autodoc",
-    # "sphinx_toolbox.more_autosummary",
-    "sphinx_toolbox.github",
-    "sphinx_toolbox.source",
-    "sphinx.ext.autodoc",
-    "sphinx_autodoc_typehints",
-    "enum_tools.autoenum",          # https://github.com/domdfcoding/enum_tools/tree/master
-    "sphinx.ext.napoleon",          # https://sphinxcontrib-napoleon.readthedocs.io/en/latest/
-    "sphinx-pydantic",
-    "sphinxcontrib.autodoc_pydantic",
-    "sphinx.ext.coverage",
-    "sphinx_copybutton",
-    "sphinx.ext.extlinks",          # https://www.sphinx-doc.org/en/master/usage/extensions/extlinks.html
-    "sphinx.ext.imgconverter",
-    "sphinx_inline_tabs",
-    "sphinx_mdinclude",
-    "sphinxemoji.sphinxemoji",      # https://sphinxemojicodes.readthedocs.io/en/stable/
-    "sphinxext.opengraph",
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+linkcheck_retries = 2
+linkcheck_timeout = 1
+linkcheck_workers = 10
+linkcheck_ignore = [
+    r"http://127\.0\.0\.1",
+    r"http://localhost",
 ]
-
 
 
 templates_path = ["_templates"]
 exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
 html_static_path = ["_static"]
@@ -80,17 +97,14 @@ html_theme_options = {
 
 latex_engine = "lualatex"  # https://github.com/readthedocs/readthedocs.org/issues/8382
 
-# See https://egitlab.gfdl.noaa.gov/NOAA-GFDL/MDTF-diagnostics/-/blob/hotfix/doc/conf.py
-# latex_additional_files = ["latex/latexmkrc"]
-
 # If true, show page references after internal links.
-# latex_show_pagerefs = True
+latex_show_pagerefs = True
 
 # If true, show URL addresses after external links.
-# latex_show_urls = "footnote"
+latex_show_urls = "footnote"
 
 # If false, no module index is generated.
-# latex_domain_indices = True
+latex_domain_indices = True
 
 # See https://www.sphinx-doc.org/en/master/latex.html
 latex_elements = {
@@ -123,36 +137,3 @@ latex_documents = [
         "manual",
     ),
 ]
-
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = True
-napoleon_use_admonition_for_notes = True
-napoleon_use_admonition_for_references = True
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-
-linkcheck_retries = 2
-linkcheck_timeout = 1
-linkcheck_workers = 10
-linkcheck_ignore = [
-    r"http://127\.0\.0\.1",
-    r"http://localhost",
-]
-
-# https://github.com/sphinx-toolbox/sphinx-toolbox/blob/master/doc-source/conf.py#L22
-# latex_elements["preamble"] = r"\usepackage{multicol}"
-# nitpicky = True
-# needspace_amount = r"4\baselineskip"
-# autodoc_type_aliases = {"ForwardRef": "ForwardRef"}
-
-
-# https://github.com/SAR-ARD/s1ard/blob/main/docs/source/conf.py
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(".."))
